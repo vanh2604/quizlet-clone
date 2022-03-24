07e40fb2d06fd4071f00c602c1f632808dc17945
dinhvietahn23 <63334287+dinhvietahn23@users.noreply.github.com>
2022-03-24 15:47:48 +0700

    standby screen: add duration to navigate to introduction screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quizlet/screens/introduction_screen.dart';
 import 'package:quizlet/screens/standby_sceen.dart';
+import 'package:quizlet/screens/test.dart';
 import 'package:quizlet/utils/colors.dart';
 
 void main() {
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       title: 'Flutter Demo',
       home: StandbyScreen(),
     );
