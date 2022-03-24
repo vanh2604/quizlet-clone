029518ad10813abbb79ae9f7dbc61161228e8d5a
vudinh2678@gmail.com <vudinh2678@gmail.com>
2022-03-24 15:14:43 +0700

    update title

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:quizlet/screens/standby_sceen.dart';
-import 'package:quizlet/utils/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Quizlet',
       home: StandbyScreen(),
     );
   }
