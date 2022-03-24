c763eec79d73c0fc50d0cfab794c2fd1a87fea45
vudinh2678@gmail.com <vudinh2678@gmail.com>
2022-03-24 16:18:24 +0700

    add fonts

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Quizlet',
-      home: StandbyScreen(),
+      theme: ThemeData(fontFamily: 'GoogleSans'),
+      home: const Scaffold(
+        body: StandbyScreen(),
+      ),
     );
   }
 }
---
+++ lib/screens/sign_up.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatelessWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text('SignUp'),
+      alignment: Alignment.center,
+    );
+  }
+}
