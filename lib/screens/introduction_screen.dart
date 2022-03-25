37ab52229b0d72861f8884304ed49a5a2fd8bc84
Ngoc Nguyen <30312571+nooeen@users.noreply.github.com>
2022-03-25 14:22:55 +0700

    Update introduction_screen.dart

--- lib/screens/introduction_screen.dart
+++ lib/screens/introduction_screen.dart
@@ -2,12 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
 final List<String> imgList = [
-  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+  'https://firebasestorage.googleapis.com/v0/b/quizzy-64099.appspot.com/o/quizlet-intro1.png?alt=media&token=aa5d9baf-699f-46a5-87d8-ac832694036a',
+  'https://firebasestorage.googleapis.com/v0/b/quizzy-64099.appspot.com/o/quizzy_intro2.png?alt=media&token=85ad2213-3322-4e2d-aaa2-8f094163deec',
+  'https://firebasestorage.googleapis.com/v0/b/quizzy-64099.appspot.com/o/quizzy_intro3.png?alt=media&token=cc638a96-6afe-41b2-a48f-670bae17afb1'
 ];
 
 final List<Widget> imageSliders = imgList
@@ -50,22 +47,53 @@ class IntroductionScreen extends StatelessWidget {
     return Container(
         color: const Color(0xff2A2D3F),
         child: Container(
-            padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+            padding: const EdgeInsets.fromLTRB(0, 25, 0, 0),
             child: Column(children: <Widget>[
+              Container(
+                padding: const EdgeInsets.fromLTRB(20, 10, 0, 30),
+                child: const Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "Quizzy",
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      decoration: TextDecoration.none,
+                    ),
+                  ),
+                ),
+              ),
               SizedBox(
-                height: 200,
+                height: 300,
                 child: CarouselSlider(
                   options: CarouselOptions(
-                    height: 200,
+                    height: 300,
                     autoPlay: true,
                     autoPlayInterval: const Duration(seconds: 3),
                     autoPlayAnimationDuration:
                         const Duration(milliseconds: 800),
                     autoPlayCurve: Curves.fastOutSlowIn,
                     enlargeCenterPage: true,
-                    aspectRatio: 2.0,
                   ),
                   items: imageSliders,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(0, 50, 10, 20),
+                child: const Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Search millions of \n flashcard sets",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      decoration: TextDecoration.none,
+                    ),
+                  ),
                 ),
               ),
             ])));
