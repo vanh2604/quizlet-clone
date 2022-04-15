f837a80f7ab69ef753b07227073ef29da23bc1cc
dinhvietahn23 <63334287+dinhvietahn23@users.noreply.github.com>
2022-04-15 14:29:19 +0700

    learn_screen(multiple choice), write_screen (fill the blank question)

---
+++ lib/screens/set/learn/result_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'learn_screen.dart';
+
+class ResultScreen extends StatefulWidget {
+  int score;
+  ResultScreen(this.score, {Key? key}) : super(key: key);
+
+  @override
+  _ResultScreenState createState() => _ResultScreenState();
+}
+
+class _ResultScreenState extends State<ResultScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(12, 12, 48, 1),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(
+            width: double.infinity,
+            child: Text(
+              "Congratulations",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 45.0,
+          ),
+          Text(
+            "You Score is",
+            style: TextStyle(color: Colors.white, fontSize: 34.0),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            "${widget.score}",
+            style: TextStyle(
+              color: Colors.orange,
+              fontSize: 85.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 100.0,
+          ),
+          FlatButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LearnScreen(),
+                  ));
+            },
+            shape: StadiumBorder(),
+            color: Colors.red,
+            padding: EdgeInsets.all(18.0),
+            child: Text(
+              "Reapeat the quizz",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
