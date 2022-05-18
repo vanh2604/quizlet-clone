0d2515cd72faf3fb5e41487110f82a7a75c6dd83
dinhvietahn23 <63334287+dinhvietahn23@users.noreply.github.com>
2022-05-19 02:01:49 +0700

    done replay for learn function

--- lib/screens/set/learn/result_screen.dart
+++ lib/screens/set/learn/result_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:quizlet/data/card_data.dart';
+import 'package:quizlet/widgets/review_term.dart';
 
 import 'learn_screen.dart';
 
-void main() => runApp(ResultScreen(10));
 
 class ResultScreen extends StatefulWidget {
   int score;
@@ -16,19 +16,22 @@ class ResultScreen extends StatefulWidget {
 class _ResultScreenState extends State<ResultScreen> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
             title:Text("Result"),
             backgroundColor: const Color.fromRGBO(52, 58, 85, 1),
         ),
         backgroundColor: const Color.fromRGBO(12, 12, 48, 1),
         body: Padding(
-          padding: const EdgeInsets.only(top: 15.0),
+          padding: const EdgeInsets.only(top: 15.0, right: 20, left: 20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              const Text("You Score",
+                style: TextStyle(color: Colors.white, fontSize: 34.0),
+              ),
+
               const SizedBox(
                 width: double.infinity,
                 child: Text(
@@ -41,47 +44,56 @@ class _ResultScreenState extends State<ResultScreen> {
                   ),
                 ),
               ),
-
-              Text(
-                "You Score",
-                style: TextStyle(color: Colors.white, fontSize: 34.0),
-              ),
-              SizedBox(
+              const SizedBox(
                 height: 20.0,
               ),
-              Text(
-                "${widget.score}",
-                style: TextStyle(
-                  color: Colors.orange,
-                  fontSize: 85.0,
-                  fontWeight: FontWeight.bold,
+
+              Container(
+                width: double.infinity,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.all(16.0),
+                    primary: Colors.white,
+                    textStyle: const TextStyle(fontSize: 20),
+                    backgroundColor: Colors.blueAccent
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => LearnScreen(
+                            questionLearn: questions,
+                          ),
+                        ));
+                  },
+                  child: const Text('Repeat the set'),
                 ),
               ),
-              SizedBox(
-                height: 100.0,
-              ),
-              FlatButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => LearnScreen(
-                          questionLearn: questions,
-                        ),
-                      ));
-                },
-                shape: StadiumBorder(),
-                color: Colors.red,
-                padding: EdgeInsets.all(18.0),
-                child: Text(
-                  "Reapeat the quizz",
-                  style: TextStyle(color: Colors.white),
+              SizedBox(height: 20,),
+
+              Container(
+                height: MediaQuery.of(context).size.height*0.7,
+                  child: PageView.builder(
+                    itemCount: 7,
+                    scrollDirection: Axis.horizontal,
+                    controller: PageController(viewportFraction: 0.93),
+                    itemBuilder: (BuildContext context, int index) {
+                      // return Center(
+                      //     child: Text(
+                      //         index.toString(),
+                      //         style: TextStyle(color: Colors.white)
+                      //     )
+                      // );
+                      return ReviewTerm(term: index.toString(), correctAnswer: "correctAnswer");
+                    },
+                  ),
                 ),
-              ),
+
+
+
             ],
           ),
         ),
-      ),
-    );
+      );
   }
 }
