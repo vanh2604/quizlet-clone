7be606a50b79012eb48b9d7cf7179b0c7f117068
dinhvietahn23 <63334287+dinhvietahn23@users.noreply.github.com>
2022-05-19 00:14:37 +0700

    add gesture to flash and learn

--- lib/screens/set/learn/result_screen.dart
+++ lib/screens/set/learn/result_screen.dart
@@ -3,6 +3,8 @@ import 'package:quizlet/data/card_data.dart';
 
 import 'learn_screen.dart';
 
+void main() => runApp(ResultScreen(10));
+
 class ResultScreen extends StatefulWidget {
   int score;
   ResultScreen(this.score, {Key? key}) : super(key: key);
@@ -14,64 +16,71 @@ class ResultScreen extends StatefulWidget {
 class _ResultScreenState extends State<ResultScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromRGBO(12, 12, 48, 1),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(
-            width: double.infinity,
-            child: Text(
-              "Congratulations",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+            title:Text("Result"),
+            backgroundColor: const Color.fromRGBO(52, 58, 85, 1),
+        ),
+        backgroundColor: const Color.fromRGBO(12, 12, 48, 1),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                width: double.infinity,
+                child: Text(
+                  "13%",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-            ),
+
+              Text(
+                "You Score",
+                style: TextStyle(color: Colors.white, fontSize: 34.0),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                "${widget.score}",
+                style: TextStyle(
+                  color: Colors.orange,
+                  fontSize: 85.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 100.0,
+              ),
+              FlatButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LearnScreen(
+                          questionLearn: questions,
+                        ),
+                      ));
+                },
+                shape: StadiumBorder(),
+                color: Colors.red,
+                padding: EdgeInsets.all(18.0),
+                child: Text(
+                  "Reapeat the quizz",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
           ),
-          SizedBox(
-            height: 45.0,
-          ),
-          Text(
-            "You Score is",
-            style: TextStyle(color: Colors.white, fontSize: 34.0),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Text(
-            "${widget.score}",
-            style: TextStyle(
-              color: Colors.orange,
-              fontSize: 85.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(
-            height: 100.0,
-          ),
-          FlatButton(
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => LearnScreen(
-                      questionLearn: questions,
-                    ),
-                  ));
-            },
-            shape: StadiumBorder(),
-            color: Colors.red,
-            padding: EdgeInsets.all(18.0),
-            child: Text(
-              "Reapeat the quizz",
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
