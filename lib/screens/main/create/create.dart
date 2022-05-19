8534eb9ffd165112b2e22237b9d78009bcd46a8b
Ngoc Nguyen <30312571+nooeen@users.noreply.github.com>
2022-05-19 23:02:41 +0700

    implement new main appbar

--- lib/screens/main/create/create.dart
+++ lib/screens/main/create/create.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:quizlet/utils/colors.dart';
 
 class CreateScreen extends StatefulWidget {
   const CreateScreen({Key? key}) : super(key: key);
@@ -13,9 +14,33 @@ class CreateScreen extends StatefulWidget {
 class _CreateScreenState extends State<CreateScreen> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => ocr(),
-      child: const Text('OCR'),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(20, 8, 20, 8),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: const InputDecoration(
+                    helperText: 'Name',
+                    hintStyle:
+                        TextStyle(color: Color.fromRGBO(255, 255, 255, 0.5)),
+                    helperStyle: TextStyle(color: textColor),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: textColor),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    setState(() {});
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
