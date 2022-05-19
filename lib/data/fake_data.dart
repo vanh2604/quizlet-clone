8f2f214dd652d27a6f57bacea8a55fc083ad0358
vudinh2678@gmail.com <vudinh2678@gmail.com>
2022-05-19 10:22:47 +0700

    fix data structure

---
+++ lib/data/fake_data.dart
@@ -0,0 +1,64 @@
+import 'package:quizlet/model/card_model.dart';
+
+List<CardModel2> questions = [
+  CardModel2(
+    "How Many Whiskers does the average cat have on each side of its face ?",
+    "12",
+    ["1", "3", "12", "5,007"],
+  ),
+  CardModel2(
+    "When does a cat purr ?",
+    "When it cares for its kittens",
+    [
+      "When it cares for its kittens",
+      "When it needs confort",
+      "When it feels content",
+      "All of the above"
+    ],
+  ),
+  CardModel2(
+    "What is the averge nulber of kittens in a litter ?",
+    "3 to 5",
+    ["1 to 2", "3 to 5", "8 to 10", "12 to 14"],
+  ),
+  CardModel2(
+    "How many moons does Mars have ?",
+    "4",
+    ["1", "2", "4", "8"],
+  ),
+  CardModel2(
+    "What is Mars's nickname ?",
+    "The red planet",
+    [
+      "The red planet",
+      "The dusty planet",
+      "The hot planet",
+      "The smelly planet"
+    ],
+  ),
+  CardModel2(
+    "About How long would it take to travel to Mars ?",
+    "Eight months",
+    ["Three days", "A month", "Eight months", "Two years"],
+  ),
+  CardModel2(
+    "Mars is Named after the Roman god Mars. What is he the god of ?",
+    "War",
+    ["Fire", "Love", "Agriculture", "War"],
+  ),
+  CardModel2(
+    "Mars Is the ___ planet from the sun ?",
+    "Fourth",
+    ["Second", "Third", "Fourth", "Sixth"],
+  ),
+  CardModel2(
+    "Where did Orville and Wilbur Wright build their first flying airplane ?",
+    "Kitty Hawk, North Carolina",
+    [
+      "Paris, France",
+      "Boston, Massachusetts",
+      "Kitty Hawk, North Carolina",
+      "New York, New York"
+    ],
+  ),
+];
--- lib/model/card_model.dart
+++ lib/model/card_model.dart
@@ -3,3 +3,10 @@ class CardModel {
   Map<String, bool>? answer;
   CardModel(this.question, this.answer);
 }
+
+class CardModel2 {
+  String? question;
+  String? answer;
+  List<String>? listAnswer;
+  CardModel2(this.question, this.answer, this.listAnswer);
+}
