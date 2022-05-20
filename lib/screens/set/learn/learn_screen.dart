46ac728351351ba103ca6ac77761de6817e98f7c
Ngoc Nguyen <30312571+nooeen@users.noreply.github.com>
2022-05-20 19:55:15 +0700

    linting

--- lib/screens/set/learn/learn_screen.dart
+++ lib/screens/set/learn/learn_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 import 'package:quizlet/model/card_model.dart';
 import 'package:quizlet/screens/set/learn/result_screen.dart';
